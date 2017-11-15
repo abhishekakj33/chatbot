18a6e380b0901fee8f7944d7f182a16a28768014
Abhishek Kumar <1003007@TCSAVIS.COM>
2017-11-15 20:41:55 +0530

    misc

--- chatbot/src/environments/environment.prod.ts
+++ chatbot/src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   dialogflow: {
-    tflybot: '55a1bb22b00543c1b0ea5eefb7915e80'
+   // tflybot: '55a1bb22b00543c1b0ea5eefb7915e80'
+    tflybot: '0558d1d5736b4035b055828145fbda28'
+    
   }
 };
--- chatbot/src/environments/environment.ts
+++ chatbot/src/environments/environment.ts
@@ -6,6 +6,7 @@
 export const environment = {
   production: false,
   dialogflow: {
-    tflybot: '55a1bb22b00543c1b0ea5eefb7915e80'
+    //tflybot: '55a1bb22b00543c1b0ea5eefb7915e80'
+    tflybot: '0558d1d5736b4035b055828145fbda28'
   }
 };
